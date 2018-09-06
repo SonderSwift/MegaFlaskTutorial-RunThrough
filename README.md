5bf07a00bc5d6f970884f6a4ba7709c1a51804c1
Jacky Chen <sonderswift@gmail.com>
2018-09-05 21:31:15 -0700

    Finish Chapter 3 of tutorial. config.py and flask_wtf web forms

--- README.md
+++ README.md
@@ -2,11 +2,25 @@
 ### This repository is meant to follow this tutorial
 [Tutorial Link](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world)
 
+Getting this Repo Running
+1. Open terminal
+2. Clone repository
+	+ ```$ git clone https://github.com/SonderSwift/MegaFlaskTutorial-RunThrough.git```
+3. Enter project directory
+	+ ```$ cd MegaFlaskTutorial-RunThrough```
+4. Create a Python virtual environment to protect your computer's global Python namespace
+	+ ```$ virtualenv venv```
+5. Install this project's dependencies to the virtualenvironment
+	+ ```(venv) $ source venv/bin/activate```
+	+ ```(venv) $ pip install -r requirements.txt```
+6. Run Flask App
+	+ ```(venv) $ flask run```
+
 Chapters
-1. set up file structure
-2. learn about frontend templating (Jinja2 template engine)
-3. 
-4. 
+1. Set up file structure for a starter flask project.
+2. Learn about frontend templating (Jinja2 template engine).
+3. Separate configuration to config.py. Use wtforms and flask-WTF for form submission.
+4.
 5.
 6.
 7.
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
-from flask import render_template
+"""
+render_template: renders the template html files using jinja2 by default
+redirect: sends user to specified page
+url_for: generates adaptive URLs using internal mapping of views
+app: flask App instantiated in __init__
+LoginForm: login class
+"""
+
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +25,16 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():  # POST request and field validation works
+    	# - create message, store in flask flash
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+    	# - access in template to show message to user
+
+        # navigate to index page
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
