79cd3a79b00fe8a5f013ffd7d4c0b2c9aebff474
Jacky Chen <sonderswift@gmail.com>
2018-11-21 21:11:06 -0800

    Finished Chapter 4

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from app import app, db
+
+@app.shell_context_processor
+def make_shell_context():
+    """ Sets up context for flask shell as handy tool for debugging
+    $ flask shell
+    # These variables are automatically defined
+    app
+    db
+    User
+    Post
+    """
+    return {'db': db, 'User': User, 'Post': Post}
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
